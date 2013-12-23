0032740a8af52b5e9e3eeb4c1eec9c72b7275fd4
Scott Wall <scottwallnz@gmail.com>
2013-12-23 14:03:05 +1100

    Finish user signup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
+  require 'factory_girl_rails'
 
   # Requires supporting ruby files with custom matchers and macros, etc, # in spec/support/ and its subdirectories. Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
   RSpec.configure do |config|
